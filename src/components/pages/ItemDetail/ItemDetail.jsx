667381592171c803c7a47aec4cb522b71fe5ca18
Julian <julianmuriel510@gmail.com>
2024-10-29 21:36:54 -0300

    segunda entrega

--- src/components/pages/ItemDetail/ItemDetail.jsx
+++ src/components/pages/ItemDetail/ItemDetail.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "./itemDetails.css";
 
 const ItemDetail = ({ item }) => {
   return (
-    <div>
+    <div
+      style={{
+        border: "3px solid black",
+        height: "320px",
+        width: "300px",
+        display: "flex",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+      }}
+    >
       <h2>{item.description}</h2>
-      <img src={item.image} alt="" />
+      <img src={item.image} style={{ width: "250px", height: "250px" }} />
     </div>
   );
 };
