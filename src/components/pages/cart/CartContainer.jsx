92250a36ff606968c663e942679c2214da831ed3
Julian <julianmuriel510@gmail.com>
2024-10-31 11:55:42 -0300

    agregamos checkout

--- src/components/pages/cart/CartContainer.jsx
+++ src/components/pages/cart/CartContainer.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const CartContainer = () => {
-  return <h1> Acá el Carrito</h1>;
+  return (
+    <div>
+      <h1> Acá el Carrito</h1>
+      <Link to="/checkout">
+        <button>Finalizar Compra</button>
+      </Link>
+    </div>
+  );
 };
