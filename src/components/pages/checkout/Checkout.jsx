0e1be3c362f3d761cbed813233c39a844aa010c8
Julian <julianmuriel510@gmail.com>
2024-11-01 14:07:08 -0300

    1

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const Checkout = () => {
-  return <div>Checkout</div>;
+  const funcionDelForm = (evento) => {
+    event.preventDefault();
+    console.log("se envia");
+    console.log(evento);
+  };
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <form onSubmit={funcionDelForm}>
+        <button>comprar</button>
+      </form>
+      <button>cancelar</button>
+    </div>
+  );
 };
