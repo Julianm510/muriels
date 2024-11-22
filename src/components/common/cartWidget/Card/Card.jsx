f84f883d289c1b5bdf960e7d9e484f0446eae84b
Julian <julianmuriel510@gmail.com>
2024-11-22 17:40:39 -0300

    entrega final

--- src/components/common/cartWidget/Card/Card.jsx
+++ src/components/common/cartWidget/Card/Card.jsx
@@ -3,28 +3,6 @@ import "./card.css";
 
 export const Card = ({ title, price, id, image }) => {
   return (
-    // <div>
-    //   <div
-    //     style={{
-    //       border: "3px solid black",
-    //       height: "320px",
-    //       width: "250px",
-    //       display: "flex",
-    //       justifyContent: "space-around",
-    //       flexWrap: "wrap",
-    //     }}
-    //   >
-    //     <img src={image} style={{ width: "150px", height: "150px" }} />
-    //     <h2>Producto: {title}</h2>
-    //     <h3>Precio: {price}</h3>
-    //     <h3>Stock: {stock}</h3>
-    //     <h3>descripcion: {description}</h3>
-    //     <Link to={`/itemDetail/${id}`}>
-    //       <button>Detalles</button>
-    //     </Link>
-    //   </div>
-    // </div>
-
     <div>
       <div className="card-grid">
         <div className="card">
